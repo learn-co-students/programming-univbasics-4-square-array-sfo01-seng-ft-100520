828667920af70c687fffc27309e248d579923e44
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-08-31 18:38:02 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  i = 0 
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i]*array[i])
+    i += 1 
+  end
+  return new_array 
 end
